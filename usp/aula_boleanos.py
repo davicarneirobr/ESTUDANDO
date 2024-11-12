bc6dd7a3e02f3e8b81236ff378f9a297ec6d9fd6
Davi Carneiro <davicarneiro@ufrj.br>
2024-11-12 14:52:58 +0000

    adicionando e salvando scrip de boleanos

---
+++ usp/aula_boleanos.py
@@ -0,0 +1,7 @@
+TempoDeJoogo = int(input("Qual tempo temos já jogado?"))
+if TempoDeJoogo<=90:
+    print("Ainda tem jogo pela frente!")
+    print("Que bom! Eu adoro futebol!")
+else:
+    print("Tá acabando o jogo!!")
+    print("Apita logo juiz!!!")
