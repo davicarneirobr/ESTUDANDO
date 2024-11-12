fa6fa0a243c6569117ab88d85a2e225ba53c86ce
Davi Carneiro <davicarneiro@ufrj.br>
2024-11-12 15:02:50 +0000

    ordem

---
+++ usp/ordem.py
@@ -0,0 +1,7 @@
+a=int(input("Digite um número inteiro:"))
+b=int(input("Digite outro número inteiro:"))
+c=int(input("Digite o último número inteiro:"))
+if a<b and b<c:
+    print("crescente")
+else:
+    print("não está em ordem crescente")
