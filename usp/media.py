b97bebc8fe2784d42490fe2369a80e1a79c5b0f3
Davi Carneiro <davicarneiro@ufrj.br>
2024-11-12 15:02:10 +0000

    comintando script de media

---
+++ usp/media.py
@@ -0,0 +1,7 @@
+nota1=float(input("Qual a sua primeira nota?"))
+nota2=float(input("Qual a sua segunda nota?"))
+nota3=float(input("Qual a sua terceira nota?"))
+nota4=float(input("Qual a sua quarta nota?"))
+media=(nota1+nota2+nota3+nota4)/4
+
+print("A média aritmética é", media)
