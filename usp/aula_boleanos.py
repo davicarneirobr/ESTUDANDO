be7a593f346d07e960b2401c4d0b88660e4c67c7
Davi Carneiro <davicarneiro@ufrj.br>
2024-11-12 15:30:19 +0000

    comitando de novo

--- usp/aula_boleanos.py
+++ usp/aula_boleanos.py
@@ -5,3 +5,10 @@ if TempoDeJoogo<=90:
 else:
     print("Tá acabando o jogo!!")
     print("Apita logo juiz!!!")
+# como fazer para adicionar e comitar (salvar) no git algum arquivo
+# faça primeiro a adição
+# chegue até a pasta que vc quer, a partir da listagem de diretórios (comando ls), e depois para entrar (cd)
+# depois
+# git add nome_do_arquivo.extensão (exemplo, nesse caso: git add aula_boleanos.py)
+# agora é hora de comitar salvar
+# git commit -m "adicione uma mensagem qualquer"
