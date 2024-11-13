ef0b91e4d126ea498f08f4c588bff4c7a7023c69
Davi Carneiro <davicarneiro@ufrj.br>
2024-11-13 16:14:00 +0000

    comitando

--- soma_dos_digitos_de_um_numero.py
+++
@@ -1,7 +0,0 @@
-x = int(input("Digite um número inteiro que você deseja que se some os algarismos:"))
-soma = 0
-while ((x // 10)!=0) or ((x % 10)!=0):
-   n = x % 10
-   soma = soma + n
-   x = (x//10)
-print ("A soma dos algarismos do número digitado é", soma)
