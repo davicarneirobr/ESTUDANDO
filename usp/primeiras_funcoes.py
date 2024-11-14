f1c7bea415df7e87c321d5cc27655708701b42af
Davi Carneiro <davicarneiro@ufrj.br>
2024-11-14 17:58:58 +0000

    comit

--- usp/primeiras_funcoes.py
+++ usp/primeiras_funcoes.py
@@ -3,3 +3,8 @@ def soma (x, y):
 print (soma(10, 20))
 print (soma(-20, 100))
 print (soma(20*32, soma(3,4)))
+
+def multiplica (w,m,n):
+    return w * m * n
+
+print (multiplica(10, 20, soma(2,3)))
