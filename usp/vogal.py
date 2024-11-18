ec9f12687d3983b4ed2d45f26e6bf5315c7ee103
Davi Carneiro <davicarneiro@ufrj.br>
2024-11-18 20:14:11 +0000

    comit

--- usp/vogal.py
+++ usp/vogal.py
@@ -1,6 +1,6 @@
 vog = True
 def vogal (a):
     if a == "A" or a == "a" or a == "E" or a == "e" or a == "I" or a == "i" or a == "O" or a == "o" or a == "U" or a == "u":
-        return print (vog)
+        return vog
     else:
-        return print (not vog)
+        return not vog
