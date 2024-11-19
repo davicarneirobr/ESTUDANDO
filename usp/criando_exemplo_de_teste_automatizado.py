a2c817305f284e05a102972c5989294562d95457
Davi Carneiro <davicarneiro@ufrj.br>
2024-11-19 22:55:28 +0000

    comit

--- usp/criando_exemplo_de_teste_automatizado.py
+++ usp/criando_exemplo_de_teste_automatizado.py
@@ -1,10 +1,13 @@
 #criando de novo para fatorial
 def fatorial (n):
-    i = fat = 1
-    while i <= n:
-        fat = fat * i
-        i = i + 1
-    return fat
+    if n == 0:
+        return 1
+    else:
+        i = fat = 1
+        while i <= n:
+            fat = fat * i
+            i = i + 1
+        return fat
 
 def test_fatorial0():
     assert fatorial(0) == 1
