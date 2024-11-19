c56928e2ded5fbd572b4270177abfb14621b9b0f
Davi Carneiro <davicarneiro@ufrj.br>
2024-11-19 22:45:16 +0000

    comitando

--- usp/criando_exemplo_de_teste_automatizado.py
+++ usp/criando_exemplo_de_teste_automatizado.py
@@ -6,17 +6,17 @@ def fatorial (n):
         i = i + 1
     return fat
 
-def teste_fatorial0 ():
-    assert fatorial (0) == 1
+def teste_fatorial0():
+    assert fatorial(0) == 1
 
-def teste_fatorial1 ():
-    assert fatorial (1) == 1
+def teste_fatorial1():
+    assert fatorial(1) == 1
 
-def teste_fatorial_negativo ():
-    assert fatorial (-10) == 0
+def teste_fatorial_negativo():
+    assert fatorial(-10) == 0
 
 def teste_fatorial4():
-    assert fatorial (4) == 24
+    assert fatorial(4) == 24
 
 def teste_fatorial5():
-    assert fatorial (5) == 120
+    assert fatorial(5) == 120
