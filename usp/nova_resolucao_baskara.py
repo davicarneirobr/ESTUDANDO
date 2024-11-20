8971b7d1ec0bb2072c97a8cbf4654c102e8469ac
Davi Carneiro <davicarneiro@ufrj.br>
2024-11-20 18:02:53 +0000

    comit

---
+++ usp/nova_resolucao_baskara.py
@@ -0,0 +1,21 @@
+import math
+def delta (a, b, c):
+    return b*b-(4*a*c)
+def main():
+    a=float(input("Qual o valor de a?"))
+    b=float(input("Qual o valor de b?"))
+    c=float(input("Qual o valor de c?"))
+    imprime_raizes(a, b, c)
+
+def imprime_raizes (a,b,c):
+    if delta(a, b, c) == 0:
+        raiz1 = (-b + math.sqrt(delta(a, b, c)))/(2*a)
+        print ("A única raiz é", raiz1)
+    else:
+        if delta (a, b, c)<0:
+            print ("Essa equação não possui raízes reais.")
+        else:
+            raiz1 = (-b + math.sqrt(delta(a, b, c)))/(2*a)
+            raiz2 = (-b - math.sqrt(delta(a, b, c)))/(2*a)
+            print ("A primeira raíz é", raiz1)
+            print ("A segunda raíz é", raiz2)
