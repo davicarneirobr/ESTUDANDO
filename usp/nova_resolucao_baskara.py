72bba63e7c7b6ae5f636ffc0e2318ca07b75dd88
Davi Carneiro <davicarneiro@ufrj.br>
2024-11-20 18:18:11 +0000

    comit

--- usp/nova_resolucao_baskara.py
+++ usp/nova_resolucao_baskara.py
@@ -8,14 +8,17 @@ def main():
     imprime_raizes(a, b, c)
 
 def imprime_raizes (a,b,c):
-    if delta(a, b, c) == 0:
-        raiz1 = (-b + math.sqrt(delta(a, b, c)))/(2*a)
+    d = delta (a, b, c)
+    if d == 0:
+        raiz1 = (-b + math.sqrt(d))/(2*a)
         print ("A única raiz é", raiz1)
     else:
-        if delta (a, b, c)<0:
+        if d<0:
             print ("Essa equação não possui raízes reais.")
         else:
-            raiz1 = (-b + math.sqrt(delta(a, b, c)))/(2*a)
-            raiz2 = (-b - math.sqrt(delta(a, b, c)))/(2*a)
+            raiz1 = (-b + math.sqrt(d))/(2*a)
+            raiz2 = (-b - math.sqrt(d))/(2*a)
             print ("A primeira raíz é", raiz1)
             print ("A segunda raíz é", raiz2)
+
+main ()
