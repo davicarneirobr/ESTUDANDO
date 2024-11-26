dd3f1bc2f4e872638469c19b11b06a28239fdbb5
Davi Carneiro <davicarneiro@ufrj.br>
2024-11-26 19:22:37 +0000

    comit

--- usp/jogo_do_NIM/rascunho_da_funcao_usuario_escolhe_jogada.py
+++ usp/jogo_do_NIM/rascunho_da_funcao_usuario_escolhe_jogada.py
@@ -2,7 +2,7 @@
 
 def  usuario_escolhe_jogada (n, m):
     y = int(input("Quantas peças você vai tirar?"))
-    while y > m or y > n:
+    while y > m or y > n or y == 0:
         print("Oops! Jogada inválida! Tente de novo.")
         y = int(input("Quantas peças você vai tirar?"))
     return y
