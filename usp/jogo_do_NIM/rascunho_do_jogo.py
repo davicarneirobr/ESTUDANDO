2fcd5d1b67f176e20013618b497451a2e794b526
Davi Carneiro <davicarneiro@ufrj.br>
2024-11-26 17:20:28 +0000

    version 1

---
+++ usp/jogo_do_NIM/rascunho_do_jogo.py
@@ -0,0 +1,73 @@
+def  usuario_escolhe_jogada (n, m):
+    y = int(input("Quantas peças você vai tirar?"))
+    while y > m or y > n:
+        print("Oops! Jogada inválida! Tente de novo.")
+        y = int(input("Quantas peças você vai tirar?"))
+    return y
+
+def  computador_escolhe_jogada (n, m):
+    if n > m:
+        x = 1
+        while (n - x) % (m + 1) != 0:
+            x = x + 1
+        maior_divisor_de_x = m // x
+        x = maior_divisor_de_x * x
+        return x
+    else:
+        x = n
+        return x
+
+def partida ():
+    n = 0
+    if n == 0:
+        n = int(input("Quantas peças?"))
+        m = int(input("Limite de peças por jogada?"))
+        if n % (m + 1) == 0:
+            print ("Você começa!")
+            usuario_escolhe_jogada (n, m)
+            if y == 1:
+                print ("Voce tirou", y, "peça.")
+            else:
+                print ("Voce tirou", y, "peças.")
+            n = n - y
+            if n == 1:
+                print ("Agora resta apenas", n, "peça no tabuleiro.")
+            else:
+                print ("Agora restam", n, "peças no tabuleiro.")
+        if n % (m + 1) != 0:
+            print ("Computador começa!")
+            computador_escolhe_jogada (n, m)
+            if x == 1:
+                print ("O computador tirou", x, "peça.")
+            else:
+                print ("O computador tirou", x, "peças.")
+            n = n - x
+            if n == 1:
+                print ("Agora resta apenas", n, "peça no tabuleiro.")
+            else:
+                print ("Agora restam", n, "peças no tabuleiro.")
+    while n != 0:
+        if n % (m + 1) == 0:
+            usuario_escolhe_jogada (n, m)
+            if y == 1:
+                print ("Voce tirou", y, "peça.")
+            else:
+                print ("Voce tirou", y, "peças.")
+            n = n - y
+            if n == 1:
+                print ("Agora resta apenas", n, "peça no tabuleiro.")
+            else:
+                print ("Agora restam", n, "peças no tabuleiro.")
+        if n % (m + 1) != 0:
+            computador_escolhe_jogada (n, m)
+            if x == 1:
+                print ("O computador tirou", x, "peça.")
+            else:
+                print ("O computador tirou", x, "peças.")
+            n = n - x
+            if n == 1:
+                print ("Agora resta apenas", n, "peça no tabuleiro.")
+            else:
+                print ("Agora restam", n, "peças no tabuleiro.")
+    if n == 0:
+        print ("Fim do jogo!")
