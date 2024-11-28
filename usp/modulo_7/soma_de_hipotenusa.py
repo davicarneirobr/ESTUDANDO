fde9f68fe57382febdf609753aee9020bd45fc40
Davi Carneiro <davicarneiro@ufrj.br>
2024-11-28 17:15:51 +0000

    comit

--- usp/modulo_7/soma_de_hipotenusa.py
+++ usp/modulo_7/soma_de_hipotenusa.py
@@ -1,1 +1,14 @@
 #criar primeiro uma funcao é_hipotenusa (que testa se n é hipotenusa ou não)
+
+def é_hipotenusa (n):
+    a = 1
+    b = 1
+    while (a*a) + (b*b) <= (n*n):
+        while b<n and a<=b:
+            if ((a*a) + (b*b)) == n*n:
+                return True
+            else:
+                b = b + 1
+        a = a + 1
+        b = a + 1
+    return False
