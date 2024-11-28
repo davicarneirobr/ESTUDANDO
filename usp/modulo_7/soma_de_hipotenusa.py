1e8f062f0fb857e0d27fd7e64453b38ba56cf409
Davi Carneiro <davicarneiro@ufrj.br>
2024-11-28 17:24:25 +0000

    comit

--- usp/modulo_7/soma_de_hipotenusa.py
+++ usp/modulo_7/soma_de_hipotenusa.py
@@ -6,9 +6,20 @@ def é_hipotenusa (n):
     while (a*a) + (b*b) <= (n*n):
         while b<n and a<=b:
             if ((a*a) + (b*b)) == n*n:
-                return True
+                return n
             else:
                 b = b + 1
         a = a + 1
         b = a + 1
-    return False
+    return 0
+
+#agr temos que pensar numa forma (provavel um loop) de ir testando os "n" que servem (até 5, que é o menor):
+
+def soma_hipotenusas (n):
+    indice = 0
+    soma = 0
+    while n >= 5:
+        indice = é_hipotenusa (n)
+        soma = soma + indice
+        n = n - 1
+    return soma
