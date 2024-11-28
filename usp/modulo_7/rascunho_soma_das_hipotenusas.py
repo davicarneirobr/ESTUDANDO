bd7ae9264c4028888ad662cb599ca95557605644
Davi Carneiro <davicarneiro@ufrj.br>
2024-11-28 15:28:56 +0000

    comit

--- usp/modulo_7/rascunho_soma_das_hipotenusas.py
+++ usp/modulo_7/rascunho_soma_das_hipotenusas.py
@@ -6,3 +6,10 @@ import math
 
 def primo_entre_si(m, n):
     return math.gcd(m, n) == 1
+
+#pensamentos avulsos 2:
+#os ternos primitivos são sempre formados por 
+#a = impares na sequencia (3,5,7,9,11,13,...)
+#com b e c sendo consecutivos (c = b+1)
+#e todo a (impar) vai ter um terno!
+#outra coisa q todos esses ternos primitivos vao ter secundarios na forma (a*k, b*k, c*k) (OBVIAMENTE)
