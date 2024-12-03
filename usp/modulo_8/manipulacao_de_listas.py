c4fcfd37031cc9ca2be7cfce8a0a2e472ee9f6de
Davi Carneiro <davicarneiro@ufrj.br>
2024-12-03 23:12:43 +0000

    comit

---
+++ usp/modulo_8/manipulacao_de_listas.py
@@ -0,0 +1,11 @@
+#fatias de listas
+
+primos_cem = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+
+print (primos_cem [1:2])
+
+print (primos_cem [2:7])
+
+print (primos_cem [:12])
+
+print (primos_cem [4:])
