abb9797282c28fbb8cb0c9b584052397d519808b
Davi Carneiro <davicarneiro@ufrj.br>
2024-12-05 02:30:59 +0000

    comit

--- usp/modulo_8/exercicio_1.py
+++ usp/modulo_8/exercicio_1.py
@@ -5,7 +5,3 @@ def remove_repetidos (lista):
         if i not in lista_sem_repetidos:
             lista_sem_repetidos.append (i)
     return lista_sem_repetidos
-
-lista = [1,2,3,3,3,4,5,6,0,2,5,7,10,10,20,30]
-
-print (remove_repetidos(lista))
