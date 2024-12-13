cdab0f0e6ece8bdd7f186b6e3260be957d1f060a
Davi Carneiro <davicarneiro@ufrj.br>
2024-12-13 00:37:58 +0000

    comit

--- usp/modulo_final/rascunho_funcao_calcula_assinatura.py
+++ usp/modulo_final/rascunho_funcao_calcula_assinatura.py
@@ -1,18 +1,24 @@
-def calcula_assinatura(texto):
-    assinatura = []
-
-
 def calcula_primeira_assinatura(texto):
     texto_sem_espacos = texto.replace(" ", "")
     numero_de_caracteres = len(texto_sem_espacos)
     lista_de_palavras_do_texto = separa_palavras(texto)
     quantidade_de_palavras = len (lista_de_palavras_do_texto)
     assinatura0 = numero_de_caracteres / quantidade_de_palavras
+    return assinatura0
 
+def calcula_segunda_assinatura (texto):
+    lista_de_palavras_do_texto = separa_palavras(texto)
+    quantidade_de_palavras = len (lista_de_palavras_do_texto)
+    numero_de_palavras_diferentes = n_palavras_unicas (lista_de_palavras_do_texto)
+    assinatura1 = numero_de_palavras_diferentes / quantidade_de_palavras
+    return assinatura1
+    
 
 def calcula_assinatura(texto):
-    '''IMPLEMENTAR. Essa funcao recebe um texto e deve devolver a assinatura do texto.'''
-    pass
+    assinatura = []
+    assinatura.append (calcula_primeira_assinatura(texto))
+    assinatura.append (calcula_segunda_assinatura(texto))
+
 
 # [0] = Tamanho médio de palavra é a soma dos tamanhos das palavras dividida pelo número total de palavras
 
