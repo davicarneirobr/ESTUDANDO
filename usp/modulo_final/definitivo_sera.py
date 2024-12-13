e09cdf33d20f1c8149051a4e824f2a65e0b0f75d
Davi Carneiro <davicarneiro@ufrj.br>
2024-12-13 17:49:33 +0000

    comit2

--- usp/modulo_final/definitivo_sera.py
+++ usp/modulo_final/definitivo_sera.py
@@ -174,11 +174,14 @@ def calcula_assinatura(texto):
     return assinatura
 
 def avalia_textos(textos, ass_cp):
+    lista_de_assinaturas_de_cada_texto_inserido = []
     lista_de_valores_de_similaridade = []
     for texto in textos:
         assinatura = calcula_assinatura(texto)
-        valor_de_similaridade = compara_assinatura(assinatura, ass_cp)
-        lista_de_valores_de_similaridade.append(valor_de_similaridade)
+        lista_de_assinaturas_de_cada_texto_inserido.append (assinatura)
+    for assinatura in lista_de_assinaturas_de_cada_texto_inserido:
+        valor_de_similaridade = compara_assinatura (assinatura, ass_cp)
+        lista_de_valores_de_similaridade.append (valor_de_similaridade)
     maior_similaridade = min(lista_de_valores_de_similaridade)
     posicao_na_lista_de_valores_de_similaridade = lista_de_valores_de_similaridade.index(maior_similaridade) + 1
     return posicao_na_lista_de_valores_de_similaridade
