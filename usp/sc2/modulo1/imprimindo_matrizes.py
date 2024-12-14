267378345a3553c22bd080f869a337027afa7cf2
Davi Carneiro <davicarneiro@ufrj.br>
2024-12-14 23:11:08 +0000

    comitando

--- usp/sc2/modulo1/imprimindo_matrizes.py
+++ usp/sc2/modulo1/imprimindo_matrizes.py
@@ -3,6 +3,3 @@ def imprime_matriz(matriz):
         for coluna in linha:
             print (coluna, end=" ")
         print ("")
-
-minha_matriz = [[1, 2, 3], [4, 5, 6]]
-imprime_matriz(minha_matriz)
