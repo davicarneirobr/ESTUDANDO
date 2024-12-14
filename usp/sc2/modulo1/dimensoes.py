2ffaf49999052af7292f282b2c0d9d90689e2712
Davi Carneiro <davicarneiro@ufrj.br>
2024-12-14 18:06:55 +0000

    comitando

---
+++ usp/sc2/modulo1/dimensoes.py
@@ -0,0 +1,11 @@
+def dimensoes (matriz):
+    lin = 0
+    col = 0
+    lin = len (matriz)
+    for linha in matriz:
+        lista_da_linha = linha
+        col = len (lista_da_linha)
+    return print(lin,"X",col, sep="")
+
+dimensoes ([[1, 2, 3], [4, 5, 6]])
+        
