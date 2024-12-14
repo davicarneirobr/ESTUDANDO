03bc68898aad3752fe3d9daaf692b3056e04622c
Davi Carneiro <davicarneiro@ufrj.br>
2024-12-14 22:49:59 +0000

    comitando

--- usp/sc2/modulo1/imprimindo_matrizes.py
+++ usp/sc2/modulo1/imprimindo_matrizes.py
@@ -1,4 +1,13 @@
 def imprime_matriz(matriz):
     for linha in matriz:
+        for coluna in linha:
+            print (coluna, end="\t")
+            print ("", end="")
+
+minha_matriz = [[1, 2, 3], [4, 5, 6]]
+imprime_matriz(minha_matriz)
+
+def imprime_matriz_bonita (matriz_linear):
+    for linha in matriz_linear:
         print (linha)
         print ("", end="")
