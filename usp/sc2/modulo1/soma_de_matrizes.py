d218477a30ba83631340625099dc7ba9beeebada
Davi Carneiro <davicarneiro@ufrj.br>
2024-12-14 18:37:50 +0000

    comitando e salvando sempre

--- usp/sc2/modulo1/soma_de_matrizes.py
+++ usp/sc2/modulo1/soma_de_matrizes.py
@@ -5,12 +5,15 @@ def dimensoes (matriz):
     for linha in matriz:
         lista_da_linha = linha
         col = len (lista_da_linha)
-    return (lin,col)
+    return lin,col
 
 def soma_matrizes (m1, m2):
     dimensao_m1 = dimensoes (m1)
     dimensao_m2 = dimensoes (m2)
+    num_lin = dimensao_m1 [0]
+    num_col = dimensao_m1 [1]
     if dimensao_m1 != dimensao_m2:
         return False
     else:
-        
+        matriz_somada = []
+         
