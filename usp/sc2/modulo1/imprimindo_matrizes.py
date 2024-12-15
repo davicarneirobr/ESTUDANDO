a2eb1e0d1ba7624ba9a44f53792d2275acc36f8e
Davi Carneiro <davicarneiro@ufrj.br>
2024-12-15 01:05:56 +0000

    comit

--- usp/sc2/modulo1/imprimindo_matrizes.py
+++ usp/sc2/modulo1/imprimindo_matrizes.py
@@ -7,6 +7,3 @@ def imprime_matriz2(matriz):
         for caractere in remover:
             linha = linha.replace(caractere, "")
         print (linha)
-
-minha_matriz = [[1, 2, 3], [4, 5, 6]]
-imprime_matriz2(minha_matriz)
