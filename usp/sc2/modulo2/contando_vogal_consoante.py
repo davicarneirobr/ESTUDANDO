f6f3ce867ad30fc968bd6f57b2b4c7ac3b8fa409
Davi Carneiro <davicarneiro@ufrj.br>
2024-12-18 17:43:36 +0000

    comit

--- usp/sc2/modulo2/contando_vogal_consoante.py
+++ usp/sc2/modulo2/contando_vogal_consoante.py
@@ -1,16 +1,9 @@
-def tirando_espacos (lista_de_nomes):
-    lista_de_nome_sem_espacos_em_braco = []
-    for nome in lista_de_nomes:
-        nome_sem_espacos = nome.strip ()
-        lista_de_nome_sem_espacos_em_braco.append (nome_sem_espacos)
-    return lista_de_nome_sem_espacos_em_braco
-
 def conta_letras(frase, contar="vogais"):
     frase = frase.lower ()
     indice = 0
     vogais = 0
     consoantes = 0
-    frase_sem_espaco = tirando_espacos (frase)
+    frase_sem_espaco = frase.strip ()
     while indice < len (frase_sem_espaco):
         if frase_sem_espaco[indice] == "a" or frase_sem_espaco[indice] == "e" or frase_sem_espaco[indice] == "i" or frase_sem_espaco[indice] == "o" or frase_sem_espaco[indice] == "u":
             vogais = vogais + 1
