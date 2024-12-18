afc8a65cf7265693ecbe29cb97c676c5701d3051
Davi Carneiro <davicarneiro@ufrj.br>
2024-12-18 17:48:19 +0000

    comitando

--- usp/sc2/modulo2/contando_vogal_consoante.py
+++ usp/sc2/modulo2/contando_vogal_consoante.py
@@ -8,11 +8,10 @@ def conta_letras(frase, contar="vogais"):
         if frase_sem_espaco[indice] == "a" or frase_sem_espaco[indice] == "e" or frase_sem_espaco[indice] == "i" or frase_sem_espaco[indice] == "o" or frase_sem_espaco[indice] == "u":
             vogais = vogais + 1
         else:
-            consoantes = consoantes + 1
+            if frase_sem_espaco[indice] != " ":
+                consoantes = consoantes + 1
         indice = indice + 1
     if contar == "consoantes":
         return consoantes
     else:
         return vogais
-
-print (conta_letras("programamos em python", "consoantes"))
